686f0e1dfbb8b854e72cf6dc03850030f4903442
Travis Hugehs <travishugehs@Traviss-Mac-mini.local>
2020-12-25 05:43:01 +0000

    Added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title')
+    404
+@endsection
+
+@section('content')
+    <div class="page-not-found">
+        <div class="page-not-found__inner">
+            <h1>404</h1>
+            <p>The page you have attempted to access couldn't be found.</p>
+        </div>
+    </div>
+@endsection
